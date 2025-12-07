86989d558ca38dee7fa80fddc16b9d7920b0ac30
bndnt <67003594+bndnt@users.noreply.github.com>
2025-12-07 03:31:00 -0500

    rename to ...slug

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -3,11 +3,11 @@
 import { useRouter, useParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import Modal from "@/components/Modal/Modal";
-import NotePreview from "@/components/NotePreview/NotePreview";
+import NotePreview from "./NotePreview.client";
 import { fetchNoteById } from "@/lib/api";
 import { Note } from "@/types/note";
 
-const NotePreviewRoute = () => {
+const NotePreviewPage = () => {
   const router = useRouter();
   const params = useParams();
   const noteId = params?.id;
@@ -24,19 +24,16 @@ const NotePreviewRoute = () => {
   });
 
   if (!noteIdParam) return null;
+
   if (isLoading) return <Modal onClose={() => router.back()}>Loading...</Modal>;
   if (isError || !note)
     return <Modal onClose={() => router.back()}>Note not found</Modal>;
 
   return (
     <Modal onClose={() => router.back()}>
-      <NotePreview
-        note={note}
-        onClose={() => router.back()}
-        // fullPageLink={`/notes/${note.id}`}
-      />
+      <NotePreview note={note} onClose={() => router.back()} />
     </Modal>
   );
 };
 
-export default NotePreviewRoute;
+export default NotePreviewPage;
